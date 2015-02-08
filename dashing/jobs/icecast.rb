52275ba4f4211e7ca0fc3bc892f0d91860e62bea
Manfred Touron <m@42.am>
2015-02-08 14:07:25 +0100

    Added dashing graph widget

--- dashing/jobs/icecast.rb
+++ dashing/jobs/icecast.rb
@@ -3,10 +3,16 @@ require 'rexml/document'
 require 'uri'
 require 'pp'
 
-current_total_listeners = 0
+total_listeners_current = 0
+total_listeners_points = []
+(1..100).each do |i|
+  total_listeners_points << { x: i, y: 0 }
+end
+total_listeners_last_x = total_listeners_points.last[:x]
 
-SCHEDULER.every '2s' do
-  last_total_listeners = current_total_listeners
+
+SCHEDULER.every '5s' do
+  total_listeners_last = total_listeners_current
   uri = URI.parse('http://' + ENV['ICECAST_PORT_8000_TCP_ADDR'] + ':' + ENV['ICECAST_PORT_8000_TCP_PORT'] + '/admin/stats.xml')
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Get.new(uri.request_uri)
@@ -18,10 +24,16 @@ SCHEDULER.every '2s' do
     #puts 'source', ele.text
   end
 
-  current_total_listeners = doc.root.elements['listeners'].text.to_i
+  total_listeners_current = doc.root.elements['listeners'].text.to_i
 
   #pp response
   #puts response.body
 
-  send_event('total-listeners', { current: current_total_listeners, last: last_total_listeners })
+  send_event('total-listeners', { current: total_listeners_current, last: total_listeners_last })
+
+
+  total_listeners_points.shift
+  total_listeners_last_x += 1
+  total_listeners_points << { x: total_listeners_last_x, y: total_listeners_current }
+  send_event('total-listeners-graph', points: total_listeners_points)
 end
